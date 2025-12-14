e895ae4fe433dd16138da2392cf60cb1d6dcabe7
PI3RRE003 <vitorcruz.dev@gmail.com>
2025-12-14 20:40:25 -0300

    Protege senha do admin usando variavel de ambiente

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+# db/seeds.rb
+
+puts "🌱 Criando Admin..."
+
+# ENV['ADMIN_PASSWORD'] vai buscar a senha no cofre do Render
+senha_secreta = ENV['ADMIN_PASSWORD']
+
+if senha_secreta.blank?
+  puts "⚠️  AVISO: Senha de admin não configurada. Pulei a criação."
+else
+  User.find_or_create_by!(email: 'gisantos880@gmail.com') do |user|
+    user.nome = 'Chef Giovanna'
+    user.password = senha_secreta
+    user.password_confirmation = senha_secreta
+    user.admin = true
+    user.endereco = 'Rua da Matriz' # Se for obrigatório no seu model
+    user.telefone = '999999999'     # Se for obrigatório
+  end
+  puts "✅ Admin criado com sucesso!"
+end
