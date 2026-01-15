58aaad24772b0a8bd66255ec9a97b57ea21ea9c8
PI3RRE003 <vitorcruz.dev@gmail.com>
2026-01-15 15:22:34 -0300

    feat: helper para abrir a loja ou fechar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def loja_aberta?
+    # Busca o primeiro administrador do sistema
+    admin = User.find_by(is_admin: true)
+
+    # A loja está aberta APENAS se o admin marcou como aberta manualmente
+    admin&.loja_aberta_manual?
+  end
 end
